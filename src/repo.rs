002e549a9b8ec005640359d76ae28679cb2d28f1
Eric Petersen <eric@ericpetersen.io>
2019-03-17 23:06:31 -0700

    Mark Repo.path be a PathBuf instead of a String
    This is the beginning of an effort to bring in changes from the
    "refactoring" branch that I forgot about before publishing 0.2.0.

--- src/repo.rs
+++ src/repo.rs
@@ -1,35 +1,35 @@
 //! Git repository representation for git-global.
 
 use std::fmt;
+use std::path::PathBuf;
 
 use git2;
 
 /// A git repository, represented by the full path to its base directory.
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct Repo {
-    path: String,
+    path: PathBuf,
 }
 
 impl Repo {
     pub fn new(path: String) -> Repo {
         Repo {
-            path: path,
+            path: PathBuf::from(path),
         }
-    }
-
-    /// Returns the full path to the repo as a `String`.
-    pub fn path(&self) -> String {
-        self.path.clone()
     }
 
     /// Returns the `git2::Repository` equivalent of this repo.
     pub fn as_git2_repo(&self) -> Option<git2::Repository> {
         git2::Repository::open(&self.path).ok()
     }
+    /// Returns the full path to the repo as a `String`.
+    pub fn path(&self) -> String {
+        self.path.to_str().unwrap().to_string()
+    }
 }
 
 impl fmt::Display for Repo {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.path)
+        write!(f, "{}", self.path())
     }
 }
